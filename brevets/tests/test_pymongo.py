efe079dcbf204edd2504593386c772eab5ee6249
JoshuaJordal <josh_jordal@JPC.localdomain>
2023-03-17 14:14:56 -0700

    Fixed tests

--- brevets/tests/test_pymongo.py
+++ brevets/tests/test_pymongo.py
@@ -6,7 +6,8 @@ Write your tests HERE AND ONLY HERE.
 
 import nose    # Testing framework
 import logging
-
+import collections
+collections.Callable = collections.abc.Callable
 
 from pymongo import MongoClient
 import os
@@ -17,9 +18,11 @@ client = MongoClient('mongodb://' + os.environ['MONGODB_HOSTNAME'], 27017)
 db = client.mydb
 
 def test_brevets_insert():
+    db.races.drop()
     db.races.insert_one({"open": '2021-01-01 00:00:00', "close": '2021-02-01 00:00:00', "km": '100', "miles": '60', "brevet": '100', "time": '2021-01-01 00:00:00'})
-    assert(db.races.find({},{ "_id": 0 }) == {"open": '2021-01-01 00:00:00', "close": '2021-02-01 00:00:00', "km": '100', "miles": '60', "brevet": '100', "time": '2021-01-01 00:00:00'})
+    assert(db.races.find_one({},{ "_id": 0 }) == {"open": '2021-01-01 00:00:00', "close": '2021-02-01 00:00:00', "km": '100', "miles": '60', "brevet": '100', "time": '2021-01-01 00:00:00'})
 
 def test_brevets_retrieval():
+    db.races.drop()
     db.races.insert_one({"open": '2021-04-01 10:30:00', "close": '2021-05-01 10:30:00', "km": '350', "miles": '300', "brevet": '300', "time": '2021-03-01 12:30:00'})
-    assert(db.races.find({},{ "_id": 0 }) == {"open": '2021-04-01 10:30:00', "close": '2021-05-01 10:30:00', "km": '350', "miles": '300', "brevet": '300', "time": '2021-03-01 12:30:00'})
+    assert(db.races.find_one({},{ "_id": 0 }) == {"open": '2021-04-01 10:30:00', "close": '2021-05-01 10:30:00', "km": '350', "miles": '300', "brevet": '300', "time": '2021-03-01 12:30:00'})
